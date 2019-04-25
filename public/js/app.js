7911320ff3a2f07a7d6910c9ddeb65e052b34b4b
arianpopalyar <arian_popalyar@yahoo.com>
2019-04-25 15:47:21 +0100

    small updated

--- public/js/app.js
+++ public/js/app.js
@@ -8,7 +8,7 @@ const message2= document.querySelector('#message-2')
 weatherForm.addEventListener('submit', (e)=>{
     e.preventDefault()
     const location = search.value
-    message1.textContent = 'L oading...'
+    message1.textContent = 'Loading ...'
     message2.textContent = ''
 
     fetch('/weather?address='+location).then((response)=>{
